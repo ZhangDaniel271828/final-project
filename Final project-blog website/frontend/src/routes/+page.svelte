53d322cf197f3c3ca41a6f6a5f33c099d1bf2ac1
Feihao Shi <shifeihao@outlook.com>
2025-02-03 13:47:31 +1300

    basic frame

---
+++ Final project-blog website/frontend/src/routes/+page.svelte
@@ -0,0 +1,15 @@
+<script>
+  export let data;
+</script>
+
+<svelte:head>
+  <title>Authentication lab - Home</title>
+</svelte:head>
+
+<h1>Home</h1>
+
+{#if data.isLoggedIn}
+  <p>Hi {data.user.firstName}! Welcome to your personalized homepage!</p>
+{:else}
+  <p>Hi there! Please login to see your personalized homepage.</p>
+{/if}
