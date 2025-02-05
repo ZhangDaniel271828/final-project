2aa519930b3d0e5da3a3ee53de807012d7c4b1ce
Wozaifei223744 <yujialisha2024@163.com>
2025-02-05 15:07:19 +1300

    a

--- Final project-blog website/backend/src/sql/init-db.sql
+++ Final project-blog website/backend/src/sql/init-db.sql
@@ -14,4 +14,4 @@ INSERT INTO Users (username, password, realName, birthDate, blurb) VALUES
     ('ketchumall', '12345', 'Ash Ketchum', '1990-05-22', 'Hey there! I am Ash Ketchum from Pallet Town, dreaming of becoming a Pokémon Master.'),
     ('pokemondoctor', '67890', 'Joy Nurtureheart', '1985-03-15', 'I am Nurse Joy, devoted to nurturing and healing Pokémon at the Pokémon Center.'),
     ('growlitheisbae', 'pa55word', 'Jenny Ironwill', '1992-07-11', 'I am Officer Jenny, upholding peace and protecting Pokémon with my Growlithe.'),
-    ('1514eva', 'pokemon', 'Samuel Oak', '1950-09-14', 'I am Professor Oak, passionate about Pokémon research and helping young trainers.');
+    ('1514eva', 'pokeman', 'Samuel Oak', '1950-09-14', 'I am Professor Oak, passionate about Pokémon research and helping young trainers.');
