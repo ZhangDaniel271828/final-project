f354e854501269dd7eba2511dff46f1443846c0a
Daisygao978 <gaoye05@gmail.com>
2025-02-07 15:57:57 +1300

    daisy

---
+++ Final project-blog website/admin/swing/src/App.java
@@ -0,0 +1,9 @@
+
+import view.MainView;
+
+public class App {
+    public static void main(String[] args) {
+        // 启动应用程序
+        javax.swing.SwingUtilities.invokeLater(() -> new MainView());
+    }
+}
