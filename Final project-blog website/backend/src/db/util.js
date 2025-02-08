dae4dd82c73b8a874355c270c36d9706ca24e0b3
Feihao Shi <shifeihao@outlook.com>
2025-02-08 13:03:43 +1300

    add manager handle router

--- Final project-blog website/backend/src/db/util.js
+++ Final project-blog website/backend/src/db/util.js
@@ -1,22 +1,3 @@
-/**
- * Builds and executes an SQL UPDATE statement with the given data.
- * 
- * For example, if the tableName = "Customers", and updateData.firstName = "John", updateData.lastName = "Doe",
- * id = 3, and idColumn = "id", then the following statement will be run:
- * 
- * UPDATE Customers SET firstName = 'John', lastName = 'Doe' WHERE id = 3
- * 
- * You are welcome to use this function in your own code.
- * 
- * @author Andrew Meads
- * 
- * @param {any} db the database
- * @param {string} tableName the name of the table to update
- * @param {any} updateData the object containing the update data
- * @param {any} id the primary key
- * @param {string} idColumn the name of the primary key column. Defaults to "id".
- * @returns 
- */
 export async function updateDatabase(db, tableName, updateData, id, idColumn = "id") {
     
     const updateOperations = [];
--- Final project-blog website/backend/src/routes/api/api-users.js
+++ Final project-blog website/backend/src/routes/api/api-users.js
@@ -1,26 +1,14 @@
 import express from "express";
 import { requiresAuthentication } from "../../middleware/auth-middleware.js";
-import { updateUser } from "../../db/users-dao.js";
+import { getAllusers } from "../../db/users-dao.js";
 
 const router = express.Router();
 
-
+// handle for ordinary users
 router.get("/me", requiresAuthentication, (req, res) => {
   return res.json(req.user);
 });
 
-/**
- * Sending a PATCH request to /api/users/me will allow requesters to update the currently authenticated user's
- * firstName, lastName, password, and blurb.
- *
- * If there is no currently authenticated user, a 401 response is returned (handled by requiresAuthentication middleware).
- *
- * If the "update user" info in req.body is invalid, a 422 response is returned.
- *
- * If the user is not found, a 404 is returned (really should never happen...)
- *
- * Otherwise, the user info is updated and a 204 response is returned.
- */
 router.patch("/update", requiresAuthentication, async (req, res) => {
   try {
     const isUpdated = await updateUser(req.user.id, req.body);
@@ -29,7 +17,19 @@ router.patch("/update", requiresAuthentication, async (req, res) => {
     return res.sendStatus(422);
   }
 });
-//自动更新
+
+//handle for manager
+router.get("/all", async (req, res) => {
+  try {
+    const allUsers = await getAllusers();    
+    return res.json(allUsers);
+  }catch (error) {
+      return res.status(500);
+  }
+});
+
+
+
 export default router;
 
 
